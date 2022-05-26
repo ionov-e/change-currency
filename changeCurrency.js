75c9717b0abbc0e0b163f4fadf969b7e2712ed7b
ionov-e <inv.great@gmail.com>
2022-05-26 03:29:33 +0300

    JS заменяет содержимое блоков с ценой, конвертируя валюту

---
+++ changeCurrency.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', changeCurrency(0));
+
+function changeCurrency(tryCount) {
+
+    let dollarPricesDivs = document.getElementsByClassName('js-product-price'); // Блоки с ценами
+
+    if ((dollarPricesDivs.length >= 1) && (dollarPricesDivs[0].innerText)) { // Если выполняется - блок товаров уже отобразился
+        changeCurrencyFinal(dollarPricesDivs);
+    } else { // Если блок товаров еще не отобразился - попробуем через паузу
+        tryCount++;
+        if (tryCount < 30) { // Чтобы бесконечно не нагрузить, если нет блоков
+            setTimeout(function () { changeCurrency(tryCount) }, 100)
+        }
+    }
+}
+
+function changeCurrencyFinal(dollarPricesDivs) {
+    $.getJSON("https://www.cbr-xml-daily.ru/daily_json.js", function (data) { // Получаем курс валют
+        let rateDol = data.Valute.USD.Value; // Курс доллара
+        for (let i = 0; i < dollarPricesDivs.length; i++) {
+            let textFromDiv = dollarPricesDivs[i].innerText;
+            let textWithoutSpaces = textFromDiv.replace(/\s/g, '');
+            let numberFromDiv = Number(textWithoutSpaces);
+            let newValue = (numberFromDiv * rateDol * 1.1).toFixed(); // К курсу еще накинуто
+            dollarPricesDivs[i].innerText = newValue.replace(/(.{3})/g,"$1 ");
+        }
+    });
+}
