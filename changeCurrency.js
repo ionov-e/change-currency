371b605387e6ccaec590807ce2dc8afa24b34637
ionov-e <inv.great@gmail.com>
2022-06-05 00:14:30 +0300

    Валера крайне помог: добавлен листенер на переключение вариации товара в блоке

--- changeCurrency.js
+++ changeCurrency.js
@@ -1,28 +1,36 @@
-document.addEventListener('DOMContentLoaded', changeCurrency(0));
+let DOLLAR_RATE = 1; // Никогда не используется как 1
 
-function changeCurrency(tryCount) {
+$(document).ready(function () {
+    $.getJSON("https://www.cbr-xml-daily.ru/daily_json.js", function (data) { // Получаем курс валют
+        DOLLAR_RATE = data.Valute.USD.Value; // Записываем в "глобальную" переменную
+        document.addEventListener('DOMContentLoaded', initialCurrencyChange(0));
+    });
+})
+
+$("body").on("change", ".js-product-edition-option-variants", function () {
+    let price = $(this).closest(".js-product").find(".js-product-price").eq(0);
+    price.text(replacePrice(price.text()));
+});
+
+function initialCurrencyChange(tryCount) {  // Меняет валюту при загрузке страницы
 
     let dollarPricesDivs = document.getElementsByClassName('js-product-price'); // Блоки с ценами
 
     if ((dollarPricesDivs.length >= 1) && (dollarPricesDivs[0].innerText)) { // Если выполняется - блок товаров уже отобразился
-        changeCurrencyFinal(dollarPricesDivs);
+        for (let i = 0; i < dollarPricesDivs.length; i++) { // Меняем содержимое, конвертируя в новую валюту
+            dollarPricesDivs[i].innerText = replacePrice(dollarPricesDivs[i].innerText);
+        }
     } else { // Если блок товаров еще не отобразился - попробуем через паузу
         tryCount++;
         if (tryCount < 30) { // Чтобы бесконечно не нагрузить, если нет блоков
-            setTimeout(function () { changeCurrency(tryCount) }, 100)
+            setTimeout(function () { initialCurrencyChange(tryCount) }, 100)
         }
     }
 }
 
-function changeCurrencyFinal(dollarPricesDivs) {
-    $.getJSON("https://www.cbr-xml-daily.ru/daily_json.js", function (data) { // Получаем курс валют
-        let rateDol = data.Valute.USD.Value; // Курс доллара
-        for (let i = 0; i < dollarPricesDivs.length; i++) {
-            let textFromDiv = dollarPricesDivs[i].innerText;
-            let textWithoutSpaces = textFromDiv.replace(/\s/g, '');
-            let numberFromDiv = Number(textWithoutSpaces);
-            let newValue = (numberFromDiv * rateDol * 1.1).toFixed(); // К курсу еще накинуто
-            dollarPricesDivs[i].innerText = newValue.replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, ' ');
-        }
-    });
+function replacePrice (textPrice) {
+    let textWithoutSpaces = textPrice.replace(/\s/g, '');
+    let numberFromDiv = Number(textWithoutSpaces);
+    let newValue = (numberFromDiv * DOLLAR_RATE * 1.1).toFixed(); // К курсу еще накинуто
+    return newValue.replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, ' ');
 }
